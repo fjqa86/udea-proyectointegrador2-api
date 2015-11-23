460c131ae33b5cc4d0fefe9410e7c3ee7a19f974
Alan Arboleda <alansgod@hotmail.com>
2015-11-23 17:16:36 -0500

    Se agregan comentarios a lps servicios y funciones implementadas

--- application/controllers/Servicio.php
+++ application/controllers/Servicio.php
@@ -2,49 +2,84 @@
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Servicio extends CI_Controller{
-    
+    /*
+     * Constructor
+     */
     function __construct() {
         parent::__construct();
         $this->load->model('post');
     }
     
+/*
+ * Servicio web: obtener_productos
+ * Autor: Francisco Quintero y Alan Arboleda
+ * Tipo: GET
+ * Descripcion: devuelve la lista de todos los productos almacenados en la base
+ *              de datos, con su respectiva información asociada como precio,
+ *              cantidad en stock, etc.
+ * Parametros: ninguno
+ */
     function producto_lista() {
-        $resultado= array();
-        $posts = $this->post->get_lista_productos();
-        foreach($posts as $post){            
-            $post_postmeta['post'] = $post;
-            $post_postmeta['meta'] = $this->post->get_post_meta($post->ID);
-            $resultado[] = $post_postmeta;
-        }        
-        $data['json'] = $resultado;
-       if (!$data['json']) show_404();
-        $this->load->view('json', $data);
-    } 
-    
-    function producto_lista2() {
-        $resultado= array();
+        $resultado= array(); // Arreglo con la información de los productos
+        //
+        // Se obtiene el listado de todos los productos con su información de 
+        // identificacion y la categoria a la que pertenece
         $productos= $this->post->get_productos_por_cat();
+        
+        // Para cada producto en la lista, se obtiene la informacion meta en la
+        // base de datos. Esta informacion incluye, entre otros, los identificadores
+        // de las imagenes del producto, los precios, descuentos, colores, tallas
         foreach($productos as $producto){            
             $prods['post'] = $producto;
-            $prods['meta'] = $this->post->get_post_meta2($producto->ID);
+            $prods['meta'] = $this->post->get_post_meta($producto->ID);
             $resultado[] = $prods;
         }        
         $data['json'] = $resultado;
        if (!$data['json']) show_404();
+       // Se retorna la informacion obtenida
         $this->load->view('json', $data);
     }
     
+    /*
+    * Servicio web: comprar_carrito
+    * Autor: Alan Arboleda
+    * Tipo: POST
+    * Descripcion: registra en la base de datos toda la informacion relacionada
+    *              con la compra de los productos existentes en el carrito de
+    *              compras: la informacion de los productos y la informacion del
+    *              comprador
+    * Parametros: recibe un archivo tipo JSON con la informacion del carrito
+    */
     function comprar_carrito(){
-        
+        // Se decodifica el archivo JSON enviado al servicio
         $cart = json_decode(file_get_contents('php://input'), true);
-        $cart = (object) $cart;
+        
+        $cart = (object) $cart; // Se hace un cast a objeto para manejo de informacion
+        
+        // Se intenta registrar la informacion de la compra y se retorna verdadero
+        // si el proceso fue exitoso. Se retorna falso en caso contrario.
         $result = $this->post->post_carrito($cart); 
         echo $result;
     }
     
+    /*
+    * Servicio web: login
+    * Autor: Francisco Quintero
+    * Tipo: GET
+    * Descripcion: verifica la existencia de un usuario en la base de datos, y
+    *              la correspondencia de la contraseña asociada con la informacion
+    *              entregada en el archivo JSON
+    * Parametros: recibe un archivo tipo JSON con la informacion del usuario
+    */
     function login() {
+        // Se decodifica el archivo JSON enviado al servicio
         $user = json_decode(file_get_contents('php://input'));
+        
         $this->load->model('user');
+        
+        // Se verifica el nombre de usuario y contrasena del archivo JSON con la
+        // informacion registrada en la base de datos. Se retorna verdadero si la
+        // verificacion fue correcta
         $result = $this->user->get_by_id($user->id);
         $data['json'] = false;
         if ($result != 0) {
--- application/models/Post.php
+++ application/models/Post.php
@@ -9,17 +9,19 @@ class Post extends CI_Model {
         parent::__construct();
     }
 
+    /*
+     * Funcion de prueba para obtener todos los productos sin ningun criterio de busqueda
+     */
     function get_lista_productos() {
         $this->db->where('post_type =', "product");
         $this->db->where('ID >', 200);
         return $this->db->get('wp_posts')->result();
     }
-
-    function get_post_meta($id) {
-        $this->db->where('post_id =', $id);
-        return $this->db->get('wp_postmeta')->result();
-    }
     
+    /*
+     * Funcion que obtiene el listado de productos con su información de identificacion
+     * y con la categoria a la que pertenece
+     */
     function get_productos_por_cat() {
         $this->db->select('wp_posts.ID, wp_posts.post_author, wp_posts.post_date, '
                 . 'wp_posts.post_content, wp_posts.post_title, wp_posts.post_excerpt, '
@@ -35,7 +37,12 @@ class Post extends CI_Model {
         return $this->db->get()->result();
     }
     
-    function get_post_meta2($id) {
+    /*
+     * Funcion que obtiene la informacion meta de un producto identificado por el
+     * parametro $id. Esta informacion incluye el precio, talla y color si aplica,
+     * cantidades disponibles.
+     */
+    function get_post_meta($id) {
         $this->db->where('post_id =', $id);
         $ids = array(2908, 2909, 2937, 2938, 2939, 2940, 2943, 2944, 2945, 2946,
             2951, 2952, 2955, 2957, 2959, 2962, 3373, 3374, 3375, 3376, 3377, 3378,
@@ -44,9 +51,13 @@ class Post extends CI_Model {
         return $this->db->get('wp_postmeta')->result();
     }
     
+    /*
+     * Funcion para almacenar en la base de datos, toda la informacion relacionada
+     * con la compra de los productos del carrito de compras
+     */
     function post_carrito($cart){
                 
-        $this->db->trans_begin();
+        $this->db->trans_begin(); // Para garantizar inregridad de la base de datos
         
         // Se insertar la orden en la tabla wp_post
         $data = array(
@@ -66,9 +77,15 @@ class Post extends CI_Model {
         
         $this->insert_postmeta($orderID,$cart); //se insertan los detalles de la orden
         
-        $jsonm = $cart->item;
+        // Se obtiene la informacion de los productos del archivo JSON
+        $jsonm = $cart->item; 
+        
+        // Se inserta en la base de datos la informacion de cada producto del
+        // carrito relacionada con la orden
         foreach($jsonm as $item) {
             $item = (object) $item;
+            
+            // Se inserta cada producto de la orden
             $data = array(
                'order_item_name' => $item->name ,
                'order_item_type' => 'line_item' ,
@@ -77,6 +94,8 @@ class Post extends CI_Model {
             $this->db->insert('wp_woocommerce_order_items', $data);
             $orderItemID = mysql_insert_id();
             
+            // Se inserta la informacion meta de cada producto de la orden
+            // Incluye informacion de identificacion, precio, talla, etc
             $data = array(
                 array(
                     'order_item_id' => $orderItemID ,
@@ -110,9 +129,12 @@ class Post extends CI_Model {
                 )
             );
             $this->db->insert_batch('wp_woocommerce_order_itemmeta', $data);
+            
+            // Se actualiza la cantidad de unidades disponibles del producto
             $this->update_Stock($item->productID,$item->quantity);
         }
 
+        // En caso de error en la transaccion se rechaza la operacion
         if ($this->db->trans_status() === FALSE) {
             $this->db->trans_rollback();
             return false;
@@ -122,8 +144,16 @@ class Post extends CI_Model {
         }
     }
   
+    /*
+     * Funcion para actualizar la cantidad de unidades disponibles en stock de un
+     * producto identificado con el id $productID que acaba de ser comprado en 
+     * una cantidad $quantity
+     */
     function update_Stock($productID, $quantity){
         
+        // Se obtiene la cantidad actual disponible del producto y se realiza
+        // la resta con la cantidad comprada para determinar la nueva cantidad
+        // disponible
         $quantityInt = (int) $quantity;
         $this->db->select('meta_value');
         $this->db->where('post_id =', $productID);
@@ -131,14 +161,24 @@ class Post extends CI_Model {
         $actualQty = (int) $this->db->get('wp_postmeta')->result();
         $newQty = $actualQty - $quantityInt;
         
+        // Si no quedan unidades disponibles se cambia el estado del producto a
+        // "outofstock"
         if ($newQty == 0){
             $this->db->where('post_id', $productID);
             $this->db->update('wp_postmeta', array('_stock_status' => 'outofstock'));
         }
+        
+        // Se actualiza la cantidad disponible del producto
         $this->db->where('post_id', $productID);
         $this->db->update('wp_postmeta', array('_stock' => $newQty)); 
     }
     
+    /*
+     * Funcion para actualizar la informacion meta de una orden identificada con
+     * el id $orderID, que esta asociada a la compra del carrito $cart. Esta 
+     * informacion incluye el tipo de moneda, la direccion destino de envio, el
+     * metodo de pago, impuestos y precio total
+     */
     function insert_postmeta($orderID, $cart){ 
         //Se inserta cada detalle de la orden en la tabla wp_postmeta
         $data = array(

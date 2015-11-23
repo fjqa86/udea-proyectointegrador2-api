1655a40373dfe1072ff22b3b275b0403f5478c7b
Alan Arboleda <alansgod@hotmail.com>
2015-11-23 17:23:12 -0500

    Se agregan comentarios al módulo User

--- application/models/User.php
+++ application/models/User.php
@@ -8,6 +8,10 @@ class User extends CI_Model {
         parent::__construct();
     }
     
+    /*
+     * Funcion para obtener la informacion de un usuario identificado con el id
+     * $id
+     */
     function get_by_id($id) {
         $this->db->where('user_login =', $id); 
         $query = $this->db->get('wp_users');
@@ -17,6 +21,9 @@ class User extends CI_Model {
         return 0;
     }
     
+    /*
+     * Funcion para obtener la informacion de un usuario a partir de su email
+     */
     function get_by_email($email) {
         $this->db->where('user_email =', $email);        
         return $this->db->get('wp_users')->result();
